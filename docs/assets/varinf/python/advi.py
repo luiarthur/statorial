8d13fb826091f6daac6ec358583d721b43bb627d
Arthur Lui <luiarthur@gmail.com>
2020-01-06 18:07:44 -0700

    using tqdm and removed ModelParam.py for simplicity

--- docs/assets/varinf/python/advi.py
+++ docs/assets/varinf/python/advi.py
@@ -7,16 +7,25 @@ torch.set_default_dtype(torch.float64)
 class ModelParam():
     def __init__(self, size, m=None, log_s=None):
         if m is None:
+            # Set the mean for the unconstrained variational distribution.
             m = torch.randn(size)
 
         if log_s is None:
+            # Set the log standard deviation  for the unconstrained variational
+            # distribution.
             log_s = torch.randn(size)
 
+        # Variational parameters
         self.vp = torch.stack([m, log_s])
         self.vp.requires_grad = True
+
+        # Dimension of the variational parameters
         self.size = size
 
     def dist(self):
+        # Unconstrained variational distribution -- a Gaussian always.
+        # NOTE: If the parameter (theta) of interest has positive support,
+        #       we place a Normal variational distribution on log(theta).
         return torch.distributions.Normal(self.vp[0], self.vp[1].exp())
 
     def rsample(self, n=torch.Size([])):
@@ -27,6 +36,8 @@ class ModelParam():
         return self.dist().rsample(n)
 
     def log_q(self, real):
+        # log variational density of parameter, evaluated on unonstrained
+        # space.
         return self.dist().log_prob(real).sum()
 
 
